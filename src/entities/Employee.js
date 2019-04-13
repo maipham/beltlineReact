40abedc833e2db8014bff5e232074e0dc2b084db
mpham33 <mpham33@gatech.edu>
2019-04-13 13:40:08 -0400

    created scripts for generating components

---
+++ src/entities/Employee.js
@@ -0,0 +1,70 @@
+import {status, user_type} from "./enums";
+import {User} from "./User";
+
+export class Employee extends User{
+    constructor(username, email, password, fname, lname, phone, emp_type, address, is_visitor) {
+        super(username, email, password, fname, lname);
+        this._emp_type = emp_type;
+        this._phone = phone;
+        this._address = address;
+        this._is_visitor = is_visitor;
+    }
+
+    set fname(new_fname) {
+        this._fname = new_fname;
+    }
+    set lname(new_lname) {
+        this._lname = new_lname;
+    }
+    set phone(new_phone) {
+        this._phone = new_phone;
+    }
+    set email(new_email) {
+        this._email.append(new_email);
+    }
+
+    set isVisitor(is_visitor) {
+        this._is_visitor = is_visitor;
+    }
+
+    get username() {
+        return this._username;
+    }
+    get password() {
+        return this._password;
+    }
+    get fname() {
+        return this._fname;
+    }
+    get lname() {
+        return this._lname;
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    remove_email(rm_email) {
+        this._email = rm_email;
+    }
+
+    get userType() {
+        return user_type.EMP;
+    }
+
+    get phone() {
+        return this._phone;
+    }
+
+    get is_visitor() {
+        return this._is_visitor;
+    }
+
+    get emp_type() {
+        return this._emp_type;
+    }
+}
---
+++ src/entities/enums.js
@@ -0,0 +1,18 @@
+export const status = {
+    PENDING: 'Pending',
+    APPROVED: 'Approved',
+    DECLINED: 'Decline'
+};
+
+export const user_type = {
+    USER: 'User',
+    VISITOR: 'Visitor',
+    EMP: 'Employee'
+
+};
+
+export const employee_type = {
+    ADMIN: 'Admin',
+    MANAGER: 'Manager',
+    STAFF: 'Staff'
+};
