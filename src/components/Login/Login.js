c3230710dd5b097d94fb3fd50e5afaad4ff44c83
mpham33 <mpham33@gatech.edu>
2019-04-19 19:23:40 -0400

    decouple a few things

---
+++ src/components/Login/Login.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {Button} from './Button/Button.js';
+import LoginForm from "./forms/LoginForm/LoginForm";
+import Grid from "@material-ui/core/Grid";
+
+export class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            password: ''
+        };
+    }
+
+    handleLogin = (val) => {
+        // console.log(val);
+        const hr = new XMLHttpRequest();
+        const url = 'http://localhost:5000/validate_login?';
+        const email = 'email=' + val.email;
+        const password = 'password=' + val.password;
+        hr.open('GET', url + email + '&' + password);
+        hr.onreadystatechange = (e) => {
+            console.log(e);
+            if (e.target.readyState === 4 && e.target.status === 200) {
+                const response = JSON.parse(e.target.responseText);
+                console.log(response);
+            }
+        };
+        hr.send();
+    };
+
+
+    render() {
+        return (
+            <div>
+                <Grid container justify="center" item xs={12}><h1>Login</h1></Grid>
+                <LoginForm onSubmit={this.handleLogin}/>
+            </div>
+        );
+    }
+}
