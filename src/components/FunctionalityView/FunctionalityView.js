08ff4be8638335d8ad02f5b501c595c8f8aad656
mpham33 <mpham33@gatech.edu>
2019-04-21 16:43:21 -0400

    function

--- src/components/FunctionalityView/FunctionalityView.js
+++ src/components/FunctionalityView/FunctionalityView.js
@@ -1,5 +1,4 @@
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './FunctionalityView.css';
 import Button from "@material-ui/core/es/Button/Button";
 import {view_options} from "../../entities/ViewOptions";
@@ -12,7 +11,9 @@ export default class FunctionalityView extends Component {
     isStaff = false;
     isManager = false;
     isVisitor = false;
-    isUser = false;
+    isUser = true;
+    isEmployee = false;
+
     constructor(props) {
         super(props);
         this.state = {
@@ -22,18 +23,11 @@ export default class FunctionalityView extends Component {
         console.log(props.location.hash);
     }
 
-
     showFunctionalityOps(hash) {
-        switch (hash) {
-            case "#admin":
-                this.isAdmin = true;
-            case "#admin":
-            case "#admin":
-            case "#admin":
-            default:
-        }
-    }
+        const hr = new XMLHttpRequest();
+        // const url = 'http://localhost:5000/?';
 
+    }
 
 
     render() {
@@ -57,13 +51,98 @@ export default class FunctionalityView extends Component {
                 <Grid container={true}
                       justify='center'
                       spacing={16}>
-                    {this.state.view_options.map((opt, i) => {
-                        return <Grid key={i} item={true} container justify='center' style={item} xs={4}>
-
-                            <Button variant="contained" style={button} component={Link} to={opt.opt[1]}>{opt.opt[0]}</Button>
+                    {this.isEmployee ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/manage_profile'}>Manage
+                                Profile</Button>
                         </Grid>
-                    })}
+                        : null
+                    }
+                    {this.isAdmin ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/manage_user'}>Manage
+                                Users</Button>
+                        </Grid>
+                        : null
+                    }
+
+                    {this.isAdmin ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/manage_transit'}>Manage
+                                Transit</Button>
+                        </Grid>
+                        : null
+                    }
+
+                    {this.isAdmin ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/manage_site'}>Manage
+                                Site</Button>
+                        </Grid>
+                        : null
+                    }
+                    {this.isManager ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/manage_event'}>Manage
+                                Event</Button>
+                        </Grid>
+                        : null
+                    }
+
+                    {this.isManager ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/manage_staff'}>View
+                                Staff</Button>
+                        </Grid>
+                        : null
+                    }
+                    {this.isManager ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/site_report'}>View Site
+                                Report</Button>
+                        </Grid>
+                        : null
+                    }
+
+                    {this.isStaff ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/view_schedule'}>View
+                                Schedule</Button>
+                        </Grid>
+                        : null
+                    }
+                    {this.isVisitor ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/explore_event'}>Explore
+                                Event</Button>
+                        </Grid>
+                        : null
+                    }
+                    {this.isVisitor ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/explore_site'}>Explore
+                                Site</Button>
+                        </Grid>
+                        : null
+                    }
+                    {this.isVisitor ?
+                        <Grid item={true} container justify='center' style={item} xs={4}>
+                            <Button variant="contained" style={button} component={Link} to={'/visit_history'}>View Visit
+                                History</Button>
+                        </Grid>
+                        : null
+                    }
+                    <Grid item={true} container justify='center' style={item} xs={4}>
+                        <Button variant="contained" style={button} component={Link} to={'/take_transit'}>Take
+                            Transit</Button>
+                    </Grid>
+                    <Grid item={true} container justify='center' style={item} xs={4}>
+                        <Button variant="contained" style={button} component={Link} to={'/transit_history'}>View Transit
+                            History</Button>
+                    </Grid>
                 </Grid>
+
+
             </div>
         );
     }
