9b35876883c4a28d210ded1a995707f76a04a0c0
Reece Gao <rgao@gatech.edu>
2019-04-21 15:39:50 -0400

    adminManageUser post of decline and approve don't update table

--- src/components/AdminManageUser/AdminManageUser.js
+++ src/components/AdminManageUser/AdminManageUser.js
@@ -17,6 +17,8 @@ const status = ["Approved", "Pending", "Declined"];
 const adminUser = "james.smith";
 
 export class AdminManageUser extends Component {
+    hr = new XMLHttpRequest();
+    url = 'http://localhost:5000/a_manage_user';
     constructor(props) {
         super(props)
         this.state = {
@@ -64,20 +66,23 @@ export class AdminManageUser extends Component {
     };
 
     handleRowClick = (event, i) => {
-        this.setState({
-            selected: i
-        })
+        if (this.state.selected === i) {
+            this.setState({
+                selected: null
+            });
+        } else {
+            this.setState({
+                selected: i
+            })
+        }
     };
 
     isSelected = id => id === this.state.selected;
 
     componentDidMount() {
-        const hr = new XMLHttpRequest();
-        const url = 'http://localhost:5000/a_manage_user?';
+        this.hr.open('GET', this.url);
 
-        hr.open('GET', url + "username=" + adminUser);
-
-        hr.onreadystatechange = (event) => {
+        this.hr.onreadystatechange = (event) => {
             if (event.target.readyState === 4 && event.target.status === 200) {
                 const data = JSON.parse(event.target.responseText);
                 this.setState({
@@ -89,7 +94,38 @@ export class AdminManageUser extends Component {
             }
         };
 
-        hr.send();
+        this.hr.send();
+    }
+
+    handleApproveClick = (event) => {
+        if (this.state.selected !== null) {
+            if (this.state.initialUsers[this.state.selected].status !== 'Approved') {
+                this.hr.open('POST', this.url);
+                this.hr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                this.hr.onreadystatechange = (event) => {
+                    if (event.target.readyState === 4 && event.target.status === 200) {
+                        let filtered = this.state.filteredUsers;
+                        filtered[this.state.selected].status = 'Approved';
+                        console.log("HELP");
+                        this.setState({
+                            filteredUsers: filtered
+                        });
+                    }
+                };
+                const username = this.state.initialUsers[this.state.selected].username;
+                // const body = {'username': username, 'status': 'Approved'};
+                this.hr.send(JSON.stringify({'username': username, 'status': 'Approved'}));
+                console.log('die me pls');
+            }
+        }
+    };
+
+    handleDeclineClick = (event) => {
+        if (this.state.selected !== null) {
+            if (this.state.initialUsers[this.state.selected].status === 'Declined') {
+                console.log('aasdfdsa');
+            }
+        }
     }
 
     render() {
@@ -144,11 +180,11 @@ export class AdminManageUser extends Component {
                     {/*Container to hold all the buttons, including filter, approve, and decline*/}
                     <Grid container justify="center" style={{marginTop: '20px'}}>
                         <Grid item style={{marginRight: '150px'}}>
-                            <Button color="primary" variant="contained">Filter</Button>
+                            <Button onClick={this.handleDeclineClick} color="primary" variant="contained">Filter</Button>
                         </Grid>
 
                         <Grid item>
-                            <Button color="primary" variant="contained" style={{marginRight: '10px'}}>Approve</Button>
+                            <Button color="primary" onClick={this.handleApproveClick} variant="contained" style={{marginRight: '10px'}}>Approve</Button>
                             <Button color="primary" variant="contained">Decline</Button>
                         </Grid>
                     </Grid>
