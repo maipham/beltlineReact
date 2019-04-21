41b05d1e905abf63b3d5972018fe3779449957c0
mpham33 <mpham33@gatech.edu>
2019-04-21 02:00:35 -0400

    mock for sitereport

--- src/entities/DailyDetail.js
+++ src/entities/DailyDetail.js
@@ -1,20 +1,24 @@
 
 export class DailyDetail {
-    constructor(event_name, staff_names, visits, revenue) {
-        this._ename = event_name;
-        this._snames = staff_names;
-        this._visits = visits;
+    constructor(date, events, staffs, totalVisits,revenue) {
+        this._date = date;
+        this._events = events;
+        this._staffs = staffs;
+        this._totalVisits = totalVisits;
         this._revenue = revenue;
     }
 
-    get event_name() {
-        return this._ename;
+    get date() {
+        return this._date;
     }
-    get staff_name() {
-        return this._snames;
+    get events() {
+        return this._events;
     }
-    get visits() {
-        return this._visits;
+    get stafs() {
+        return this._staffs;
+    }
+    get totalVisits() {
+        return this._totalVisits;
     }
     get revenue() {
         return this._revenue;
